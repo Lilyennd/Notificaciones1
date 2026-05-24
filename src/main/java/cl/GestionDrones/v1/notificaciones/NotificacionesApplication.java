5e25c65c8d401d310c1a65d1f017cf15a99fb486
Lilyennd <victoriariffo21@gmail.com>
2026-05-24 13:32:49 -0400

    inicio

---
+++ src/main/java/cl/GestionDrones/v1/notificaciones/NotificacionesApplication.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.notificaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacionesApplication.class, args);
+	}
+
+}
